052fc5bfd00067cc0f79b3a30b008cfe49f30db3
patrick <patrick@Xyzzytop.lan>
2008-10-23 01:58:47 -0700

    More

--- p213.c
+++ p213.c
@@ -5,9 +5,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
+#define side    30
 
-    printf("%d",sizeof(double));
+int main(int argc, char **argv) {
+    int i;
+    double ***board = malloc(side*side*side*side*2*sizeof(double))
 
     return(0);
+}
+
+updateOneBoard(double *src, double *dst)
+{
+    int i,j;
+    double *pUp, *pDown;
+    double *pSrc = src;
+    double *pDst = dst;
+
+    /* First handle the top row separately */
+    pDown = pSrc + side;
+    *pDst++ = (1/2.0) * (*(pSrc+1) + *pDown++);
+    pSrc++;
+    for(j = 1; j < side-1; ++j) {
+        *pDst++ = (1/3.0) * (*(pSrc-1) + *(pSrc+1) + *pDown++);
+        pSrc++;
+    }
+    *pDst++ = (1/2.0) * (*(pSrc-1) + *pDown++);
+    pSrc++;
+    for(i = 1; i < side-1; ++i) {
+        /* Handle left side */
+        *pDst++ = (1/3.0) * (*pUp++ + *pDown++ + *(pSrc+1));
+        for(j = 1; j < side-1; ++j) {
+
+        }
+    }
 }
