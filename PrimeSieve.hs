daf60a3bb1de1f6073ffdb323caa9f3ed4829cdc
Patrick Carlisle <patcarlisle@gmail.com>
2008-10-26 22:57:46 -0700

    Make PrimeSieve work on Int and Integer, add SPECIALIZE directive for both.

--- PrimeSieve.hs
+++ PrimeSieve.hs
@@ -1,23 +1,43 @@
 module PrimeSieve (primes) where
 
-data Wheel = Wheel Int [Int] deriving Show
+data (Integral a) => Wheel a = Wheel a [a] deriving Show
 
+{-# SPECIALIZE wheels :: [Wheel Int] #-}
+{-# SPECIALIZE wheels :: [Wheel Integer] #-}
+wheels :: (Integral a) => [Wheel a]
 wheels = 
   Wheel 1 [1] :
   zipWith nextSize wheels primes
 
+{-# SPECIALIZE nextSize :: Wheel Int -> Int -> Wheel Int #-}
+{-# SPECIALIZE nextSize :: Wheel Integer -> Integer -> Wheel Integer #-}
+nextSize :: (Integral a) => Wheel a -> a -> Wheel a
 nextSize (Wheel s ns) p =
   Wheel (s*p) [n' | o <- [0, s..(p-1)*s],
                     n <- ns,
                     n' <- [n+o], n' `mod` p > 0]
 
-primes = sieve wheels primes squares
-  where
-    sieve ((Wheel s ns) : ws) ps qs =
-      [n' | o <- s : [2*s, 3*s..(head ps-1)*s],
-            n <- ns,
-            n' <- [n+o], s <= 2 || noFactorIn ps qs n' ]
-      ++ sieve ws (tail ps) (tail qs)
-    noFactorIn (p:ps) (q:qs) x = q > x || x `mod` p > 0 && noFactorIn ps qs x
-    squares = [p*p | p <- primes]
+{-# SPECIALIZE sieve :: [Wheel Int] -> [Int] -> [Int] -> [Int] #-}
+{-# SPECIALIZE sieve :: [Wheel Integer] -> [Integer] -> [Integer] -> [Integer] #-}
+sieve :: (Integral a) => [Wheel a] -> [a] -> [a] -> [a]
+sieve ((Wheel s ns) : ws) ps qs =
+  {-# SCC "Woot" #-} [n' | o <- {-# SCC "one" #-} s : [2*s, 3*s..(head ps-1)*s],
+                           n <- {-# SCC "two" #-} ns,
+                           n' <- [n+o], s <= 2 || {-# SCC "three" #-} noFactorIn ps qs n' ]
+  ++ sieve ws (tail ps) (tail qs)
+{- The problem here is noFactorIn is doing trial division.  There shouldn't be any of that -}
 
+{-# SPECIALIZE noFactorIn :: [Int] -> [Int] -> Int -> Bool #-}
+{-# SPECIALIZE noFactorIn :: [Integer] -> [Integer] -> Integer -> Bool #-}
+noFactorIn :: (Integral a) => [a] -> [a] -> a -> Bool
+noFactorIn (p:ps) (q:qs) x = {-# SCC "noFactorIn" #-} q > x || x `mod` p > 0 && noFactorIn ps qs x
+
+{-# SPECIALIZE squares :: [Int] #-}
+{-# SPECIALIZE squares :: [Integer] #-}
+squares :: (Integral a) => [a]
+squares = [p*p | p <- primes]
+
+{-# SPECIALIZE primes :: [Int] #-}
+{-# SPECIALIZE primes :: [Integer] #-}
+primes :: (Integral a) => [a]
+primes = sieve wheels primes squares
